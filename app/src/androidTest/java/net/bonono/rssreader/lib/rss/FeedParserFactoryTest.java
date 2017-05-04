31ed23d289b952a1adb7f58f09784e49a64b3ddc
bonono <bonono.jp@gmail.com>
2017-05-05 02:00:04 +0900

    Changed argument type of FeedParserFactory.newInstanceFor

--- app/src/androidTest/java/net/bonono/rssreader/lib/rss/FeedParserFactoryTest.java
+++ app/src/androidTest/java/net/bonono/rssreader/lib/rss/FeedParserFactoryTest.java
@@ -8,32 +8,25 @@ import org.junit.runner.RunWith;
 import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.*;
 
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
-
 @RunWith(AndroidJUnit4.class)
 public class FeedParserFactoryTest {
     @Test
     public void returnNullForInvalidXml() throws Exception {
-        InputStream is = new ByteArrayInputStream("<test />".getBytes());
-        assertThat(FeedParserFactory.newInstanceFor(is), is(nullValue()));
+        assertThat(FeedParserFactory.newInstanceFor("<test />"), is(nullValue()));
     }
 
     @Test
     public void createParserForRss1() throws Exception {
-        InputStream is = new ByteArrayInputStream("<rdf:RDF />".getBytes());
-        assertThat(FeedParserFactory.newInstanceFor(is), instanceOf(Rss1Parser.class));
+        assertThat(FeedParserFactory.newInstanceFor("<rdf:RDF />"), instanceOf(Rss1Parser.class));
     }
 
     @Test
     public void createParserForRss2() throws Exception {
-        InputStream is = new ByteArrayInputStream("<rss />".getBytes());
-        assertThat(FeedParserFactory.newInstanceFor(is), instanceOf(Rss2Parser.class));
+        assertThat(FeedParserFactory.newInstanceFor("<rss />"), instanceOf(Rss2Parser.class));
     }
 
     @Test
     public void createParserForAtom() throws Exception {
-        InputStream is = new ByteArrayInputStream("<feed />".getBytes());
-        assertThat(FeedParserFactory.newInstanceFor(is), instanceOf(AtomParser.class));
+        assertThat(FeedParserFactory.newInstanceFor("<feed />"), instanceOf(AtomParser.class));
     }
 }
--- app/src/main/java/net/bonono/rssreader/lib/rss/FeedParserFactory.java
+++ app/src/main/java/net/bonono/rssreader/lib/rss/FeedParserFactory.java
@@ -6,39 +6,38 @@ import android.util.Xml;
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserException;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 
 public class FeedParserFactory {
     @Nullable
-    public static FeedParser newInstanceFor(InputStream is) throws XmlPullParserException, IOException {
-        XmlPullParser parser = Xml.newPullParser();
-        parser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false);
-        parser.setInput(is, null);
-
-        // consume until first tag
-        while(parser.getEventType() != XmlPullParser.START_TAG &&
-                parser.getEventType() != XmlPullParser.END_DOCUMENT) {
-            parser.next();
-        }
-
+    public static FeedParser newInstanceFor(String s) throws XmlPullParserException, IOException {
         FeedParser fp = null;
-        if (parser.getEventType() == XmlPullParser.START_TAG) {
-            switch (parser.getName()) {
-                case "rdf:RDF":
-                    fp = new Rss1Parser();
-                    break;
-                case "rss":
-                    fp = new Rss2Parser();
-                    break;
-                case "feed":
-                    fp = new AtomParser();
-                    break;
-            }
-        }
 
-        if (is.markSupported()) {
-            is.reset();
+        try(ByteArrayInputStream ba = new ByteArrayInputStream(s.getBytes())) {
+            XmlPullParser parser = Xml.newPullParser();
+            parser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false);
+            parser.setInput(ba, null);
+
+            // consume until first tag
+            while (parser.getEventType() != XmlPullParser.START_TAG &&
+                    parser.getEventType() != XmlPullParser.END_DOCUMENT) {
+                parser.next();
+            }
+
+            if (parser.getEventType() == XmlPullParser.START_TAG) {
+                switch (parser.getName()) {
+                    case "rdf:RDF":
+                        fp = new Rss1Parser();
+                        break;
+                    case "rss":
+                        fp = new Rss2Parser();
+                        break;
+                    case "feed":
+                        fp = new AtomParser();
+                        break;
+                }
+            }
         }
 
         return fp;
