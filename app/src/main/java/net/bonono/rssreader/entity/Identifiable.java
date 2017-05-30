4c8b68ca1b5ace1286c13b897e5d1d0c266322bd
bonono <bonono.jp@gmail.com>
2017-05-30 23:48:30 +0900

    Converted Identifiable interface to Kotlin

--- app/src/main/java/net/bonono/rssreader/entity/Identifiable.java
+++
@@ -1,6 +0,0 @@
-package net.bonono.rssreader.entity;
-
-public interface Identifiable {
-    long getId();
-    void setId(long id);
-}
---
+++ app/src/main/java/net/bonono/rssreader/entity/Identifiable.kt
@@ -0,0 +1,5 @@
+package net.bonono.rssreader.entity
+
+interface Identifiable {
+    var id: Long
+}
